1249ebd67eac56f423864473ba8314a9fea2c887
Davi Souza <davimj99@gmail.com>
2026-09-03 16:10:56 -0300

    refactor(guiche): atualiza referências para entity

--- guiche-api/src/main/java/br/com/davi/guiche_api/repository/GuicheRepository.java
+++ guiche-api/src/main/java/br/com/davi/guiche_api/repository/GuicheRepository.java
@@ -1,5 +1,5 @@
 package br.com.davi.guiche_api.repository;
-import br.com.davi.guiche_api.model.Guiche;
+import br.com.davi.guiche_api.entity.Guiche;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GuicheRepository extends JpaRepository<Guiche, Long> {
